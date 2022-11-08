8a25eb1130a0cd670c79faed5b997bde59ef65da
Joris Falter <joris@jorisfalter.com>
2022-11-08 14:47:06 +0700

    next button works

--- client/src/components/PictureComponent.jsx
+++ client/src/components/PictureComponent.jsx
@@ -1,28 +1,23 @@
 import React from "react";
 
-// function sayHello() {
-//   alert("You clicked me!");
-// }
-
 function PicComponent() {
   const [data, setData] = React.useState(null);
 
-  const whenClicked = React.useEffect(() => {
+  React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
-  // const fetchRequest = React.useCallback(() => {
-  //   fetch("/api")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }, []);
+  function WhenClicked() {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }
 
   return (
     <div>
-      {/* <p>{!data ? "Loading..." : data}</p> */}
-      <button onClick={whenClicked}>Next</button>
+      <button onClick={WhenClicked}>Next</button>
       <img alt="airport" src={!data ? "Loading..." : data} />
     </div>
   );
