9bfa7c8d427694f67e49d0f57495eac76779be10
Joris Falter <joris@jorisfalter.com>
2022-11-08 15:06:23 +0700

    clean up

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-// import Button from "./Button";
 import PicComponent from "./PictureComponent.jsx";
 
 function App() {
-  // const [data, setData] = React.useState(null);
-
-  // React.useEffect(() => {
-  //   fetch("/api")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>{!data ? "Loading..." : data}</p> */}
-        {/* <Button /> */}
         <div ClassName="App">
           <PicComponent />
         </div>
-        {/* <img alt="airport" src={!data ? "Loading..." : data} /> */}
       </header>
     </div>
   );
